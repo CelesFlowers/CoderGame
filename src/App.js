43642071d26861c03da03fb083a1c72d652927fc
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-04-25 09:30:34 -0300

    defaultURL

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Favorites from "./components/Favorites/favorites";
 import VideoGames from "./components/VideoGames/videogames";
 
 import axios from 'axios'
-axios.defaults.baseURL='https://codergameback-production.up.railway.app/'
+axios.defaults.baseURL='http://localhost3001/'
 
 
 function App() {
