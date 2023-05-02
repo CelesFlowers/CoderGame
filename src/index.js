abf505db0f1b02470e8cef6d044962611b748ac0
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-05-02 16:43:28 -0300

    authclient

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Auth0Provider
-        domain="dev-nw4a48dj5goqe2ja.us.auth0.com"
-        clientId="O7A2dLNpHkT0plBUVgaliEDklNOMJ97Q"
+        domain="dev-brqs52ydufaqghca.us.auth0.com"
+        clientId="7Y5WUlfdfJn2JDvdpJOTOvirHokm9VGZ"
         authorizationParams={{
           redirect_uri: window.location.origin,
         }}
